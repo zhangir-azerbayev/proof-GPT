d66b713392b376e5aa76d5284f0209156800b270
zhangir-azerbayev <zazerbayev@gmail.com>
2022-08-03 20:37:22 -0400

    i think this now works with the old format

--- data/fetch_arxiv.py
+++ data/fetch_arxiv.py
@@ -1,6 +1,7 @@
 import os
 import sys 
 from pathlib import Path
+import datetime
 
 import tarfile
 import xml.etree.ElementTree as ET 
@@ -10,6 +11,40 @@ import re
 import shutil
 
 import arxiv 
+
+def process_tarball_old_scheme(tarball_name, save_dir): 
+    tarball_path = os.path.join(save_dir, tarball_name)
+    os.system("tar -xf " + tarball_path + " -C " + save_dir)
+
+    last_ = tarball_name.rfind("_")
+    second_last_ = tarball_name.rfind("_", 0, last_)
+    subdir = tarball_name[second_last_+1:last_]
+    
+    subpath = os.path.join(save_dir, subdir)
+    zipped_names = os.listdir(subpath)
+
+    for zipped_name in zipped_names: 
+        if zipped_name[-len(".gz"):]==".gz": 
+            zipped_path = os.path.join(subpath, zipped_name)
+            if re.match(r"math", zipped_name): 
+                if tarfile.is_tarfile(zipped_path): 
+                    os.system("tar -xzf " + zipped_path + " -C " + subpath)
+                    os.remove(zipped_path)
+                else: 
+                    os.system("gzip -d " + zipped_path)
+                    unzipped_path = os.path.join(subpath, zipped_name[:-len(".gz")])
+                    os.rename(unzipped_path, unzipped_path + ".tex")
+            else: 
+                os.remove(zipped_path)
+
+    listdir = os.listdir(subpath)
+    for fle in listdir: 
+        path = os.path.join(subpath, fle)
+        if not re.match(r".*\.tex", fle): 
+            if os.path.isfile(path): 
+                os.remove(path)
+            else: 
+                shutil.rmtree(path)
 
 def process_tarball(tarball_name, save_dir): 
     tarball_path = os.path.join(save_dir, tarball_name)
@@ -44,7 +79,7 @@ def process_tarball(tarball_name, save_dir):
             zipped_path = os.path.join(subpath, eyed + ".gz")
 
             if tarfile.is_tarfile(zipped_path): 
-                os.system("tar -xvzf " + zipped_path + " -C " + subpath)
+                os.system("tar -xzf " + zipped_path + " -C " + subpath)
                 os.remove(zipped_path)
             else: 
                 os.system("gzip -d " + zipped_path)
@@ -76,18 +111,27 @@ def main():
     root = tree.getroot()
     
     shards_to_get = []
+    yymms = []
     for child in root: 
         if child.tag == "file":
             fle_field = child[1] # the index of filename
             shards_to_get.append(fle_field.text)
+            yymms.append(child[9].text)
     # delete this line later
-    shards_to_get = [shards_to_get[1000]]
-
-    for shard in shards_to_get: 
+    print(yymms)
+    sys.exit()
+    shards_to_get = [shards_to_get[0], shards_to_get[1000]]
+    
+    format_cutoff = datetime.datetime(2007, 3, 1)
+    for shard, yymm in tqdm(zip(shards_to_get, yymms)): 
+        print("SHARD: ", shard)
         os.system(f"s3cmd get s3://arxiv/" + shard + \
                 " --requester-pays " + save_dir) 
         tarball_name=shard[shard.rindex("/")+1:]
-        process_tarball(tarball_name, save_dir)
+        if datetime.datetime(yymm[:2], yymm[2:], 1)<=format_cutoff: 
+            process_tarball_old_scheme(tarball_name, save_dir)
+        else: 
+            process_tarball(tarball_name, save_dir)
     
 if __name__=="__main__": 
     main()
