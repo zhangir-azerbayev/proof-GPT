f9fe6ee68e6eacf8717d28636fe9d5b6f9b05048
zhangir-azerbayev <zazerbayev@gmail.com>
2022-08-16 16:53:00 -0400

    yes

--- data/test_dataset.py
+++ data/test_dataset.py
@@ -2,7 +2,7 @@ from datasets import load_dataset
 import time
 from tqdm import tqdm
 
-remote_dataset = load_dataset("hoskinson-center/proof-pile", "formal", streaming=True)
+remote_dataset = load_dataset("hoskinson-center/proof-pile", "stack-exchange", streaming=True)
 
 then = time.time()
 for x in tqdm(remote_dataset["train"]): 
